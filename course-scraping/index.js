382e3ce7b8f44ede70be78e54c7c259906415509
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-07-10 03:05:41 +0530

    updated file

--- course-scraping/index.js
+++ course-scraping/index.js
@@ -6,24 +6,14 @@ async function run() {
     const page = await browser.newPage();
     await page.goto('https://traversymedia.com');
 
-    //await page.screenshot({ path: 'example.png', fullPage: true });
-    //await page.pdf({ path:'example.pdf', format: 'A4' });
-
-    //const html = await page.content();
-    
-    //const title = await page.evaluate(() => document.title);
-
-    //const text = await page.evaluate(() => document.body.innerText);
-
-    // const links=  await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
-    // console.log(links);
 
     // const courses =  await page.evaluate(() => Array.from(document.querySelectorAll('#cscourses .card'), (e) => ({
     //     title: e.querySelector('.card-body h3').innerText,
     //     level: e.querySelector('.card-body .level').innerText,
     //     url: e.querySelector('.card-footer a').href,
     // })));
-    // console.log(courses);
+
+    //another way of doing the same thing
 
     const courses = await page.$$eval('#cscourses .card', (elements) => elements.map(e =>({
         title: e.querySelector('.card-body h3').innerText,
